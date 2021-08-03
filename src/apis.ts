bea35fccbe7123e99a3d47c97e9d39d30ebbf407
sapphi-red <green@sapphi.red>
2021-08-03 15:45:55 +0900

    Fix apis

--- src/apis.ts
+++ src/apis.ts
@@ -1,8 +1,10 @@
-import { Apis } from '@traptitech/traq'
+import { Apis, Configuration } from '@traptitech/traq'
 
-export const apis = new Apis({
-  basePath: '/api/v3'
-})
+export const apis = new Apis(
+  new Configuration({
+    basePath: '/api/v3'
+  })
+)
 
 export const getFileUrl = <T extends string | undefined>(fileId: T): T =>
   (fileId ? `/api/v3/files/${fileId}` : undefined) as T
