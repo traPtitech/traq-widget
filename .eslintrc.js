736923c8e370a0503fddf09aebd8a50c419f076a
sapphi-red <green@sapphi.red>
2021-11-12 09:48:48 +0900

    eslintの設定の変更

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
+  ],
+  overrides: [
+    {
+      // 直下のファイル
+      files: ['*.{js,mjs}'],
+      excludedFiles: ['*/**/*.{js,mjs}'],
+      env: {
+        node: true
+      },
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off'
+      }
+    }
+  ],
+  reportUnusedDisableDirectives: true
+}
